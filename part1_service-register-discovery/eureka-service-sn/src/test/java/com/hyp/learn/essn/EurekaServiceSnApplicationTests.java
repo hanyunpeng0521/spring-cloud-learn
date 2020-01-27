893520ba3b6a624d13047e7fee0a2ce602650bef
hanyunpeng <m13839441583@163.com>
2020-01-27 22:44:16 +0800

    first commit

---
+++ part1_service-register-discovery/eureka-service-sn/src/test/java/com/hyp/learn/essn/EurekaServiceSnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyp.learn.essn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceSnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
